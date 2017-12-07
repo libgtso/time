d60c62274e47b12b8bf0500a8f4f6fa47f14303c
addict48 <sir_mas@mail.ru>
2017-12-07 17:48:28 +0500

    add blocks

--- src/js/main.js
+++ src/js/main.js
@@ -15,8 +15,32 @@ $(document).ready(function() {
 		navText: " ",
 		mouseDrag: false
 	});
+	$(".else__carousel").owlCarousel({
+		loop: true,
+		items: 4,
+		nav: true,
+		dots: true,
+		navText: " ",
+		mouseDrag: false
+	});
+	$(".seen-list").owlCarousel ({
+		loop: true,
+		items: 4,
+		nav: true,
+		navText: ""
+	});
+
 	$("a#watch-card").fancybox();
 	$("a#watch-card").fancybox({
 		'hideOnContentClick': true
+	});
+
+	$('.cardunit-all').on('click', function(e) {
+		e.preventDefault();
+		var allparams = $('.allparams-title').offset().top;
+		
+		$('html, body').animate({
+			scrollTop: allparams
+		}, 1000);
 	});
 });
